632e3891861ae876e0be8fc17563445691b2b166
王 <oukunn@ou-no-MacBook-Air.local>
2017-07-19 23:45:03 +0900

    Create Project

---
+++ CustomCollectionView/CustomCollectionView/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CustomCollectionView
+//
+//  Created by 王 on 2017/07/19.
+//  Copyright © 2017年 WangJun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
