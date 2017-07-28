37fbd956ea82d531872fd90de97fa0efc71a84cc
LitttleTwo_Wang <wang995101207@gmail.com>
2017-07-28 15:27:45 +0900

    Calendar
    Commit to prepare for creating Calendar View

--- CustomCollectionView/TypeCategoryCollectionViewCell.swift
+++ CustomCollectionView/TypeCategoryCollectionViewCell.swift
@@ -43,31 +43,5 @@ class TypeCategoryCollectionViewCell: UICollectionViewCell {
         shakeAnimation.autoreverses = true
         self.layer.add(shakeAnimation, forKey: "transform.rotation.z")
         
-        
-//        UIView.animateKeyframes(withDuration: 0.2, delay: 0, options: [.repeat], animations: {
-//            let transform = CGAffineTransformFromString("AAA")
-//            UIView.addKeyframe(withRelativeStartTime: 0.001, relativeDuration: 0.25, animations: {
-//                self.categoryImage.transform = transform.rotated(by: -CGFloat.pi/60)
-//                self.titleLabel.transform = transform.rotated(by: -CGFloat.pi/60)
-//                self.deleteBtn.transform = transform.rotated(by: -CGFloat.pi/60)
-//            })
-//            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25, animations: {
-//                self.categoryImage.transform = CGAffineTransformFromString("BBB")
-//                self.titleLabel.transform = CGAffineTransformFromString("BBB")
-//                self.deleteBtn.transform = CGAffineTransformFromString("BBB")
-//            })
-//            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.25, animations: {
-//                self.categoryImage.transform = transform.rotated(by: CGFloat.pi/60)
-//                self.titleLabel.transform = transform.rotated(by: CGFloat.pi/60)
-//                self.deleteBtn.transform = transform.rotated(by: CGFloat.pi/60)
-//            })
-//            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25, animations: {
-//                self.categoryImage.transform = CGAffineTransformFromString("BBB")
-//                self.titleLabel.transform = CGAffineTransformFromString("BBB")
-//                self.deleteBtn.transform = CGAffineTransformFromString("BBB")
-//            })
-//        }) { (complete) in
-//            
-//        }
     }
 }
